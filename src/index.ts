270a5e81b29bccdb1442a39d3ca1d7b442cfaa4a
tuiu <13719283454@163.com>
2026-01-15 01:35:11 +0800

    feat(index): 🎉 添加命令行界面功能
    引入 commander 库，创建基本的命令行界面结构，并设置版本号和描述。

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,16 @@
+import { Command } from "commander";
+import { version } from "../package.json";
+
 async function main() {
-  console.log("Hello, World!!!");
+  const program = new Command();
+
+  program
+    .name("laya-resource-parser")
+    .alias("lr")
+    .version(version, "-V, --version")
+    .description("Laya资源解析")
+
+  await program.parseAsync(process.argv);
 }
 
 main().catch(err => {
